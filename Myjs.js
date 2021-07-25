c4c8de331485f88bdcd539b5ac3a29bc0b82b306
Thu <nguyenthahthu1414@gmail.com>
2021-07-25 20:03:32 +0700

    Thu sua Hover

--- Myjs.js
+++ Myjs.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $("#plus-1").hide()
         $("#minus-1").show()
     })
-    $("#minus-1").click(function(){
+    $("#minus-1").click(function () {
         $("#sidedown-1").slideUp(1000)
         $("#plus-1").show()
         $("#minus-1").hide()
@@ -14,7 +14,7 @@ $(document).ready(function () {
         $("#plus-2").hide()
         $("#minus-2").show()
     })
-    $("#minus-2").click(function(){
+    $("#minus-2").click(function () {
         $("#sidedown-2").slideUp(1000)
         $("#plus-2").show()
         $("#minus-2").hide()
@@ -24,26 +24,28 @@ $(document).ready(function () {
         $("#plus-3").hide()
         $("#minus-3").show()
     })
-    $("#minus-3").click(function(){
+    $("#minus-3").click(function () {
         $("#sidedown-3").slideUp(1000)
         $("#plus-3").show()
         $("#minus-3").hide()
     })
-    $("#system-img-1").mouseenter(function(){
-        $("#system-hide-icon-1").fadeOut(500)
-        $(".system-img-item").addClass("hover-system")
+
+    $('.system-img').find('.system-img-item').hover(function () {
+        // hover IN
+        $(this).find('img').fadeOut(300);
+        $(this).css({
+            'pointer-events': 'none',
+        })
+        $(this).addClass("hover-system", 200, function () {
+            $(this).css({
+                'pointer-events': 'auto',
+            })
+        })
+    }, function () {
+        // hover Out
+        $(this).find('img').fadeIn();
+        $(this).removeClass("hover-system")
     })
-    $("#system-img-1").mouseleave(function(){
-        $("#system-hide-icon-1").fadeIn(500)
-        $(".system-img-item").removeClass("hover-system")
-    })
-    $("#system-img-2").mouseenter(function(){
-        $("#system-hide-icon-2").fadeOut(500)
-        $(".system-img-item").addClass("hover-system")
-    })
-    $("#system-img-2").mouseleave(function(){
-        $("#system-hide-icon-2").fadeIn(500)
-        $(".system-img-item").removeClass("hover-system")
-    })
+
 
 })
